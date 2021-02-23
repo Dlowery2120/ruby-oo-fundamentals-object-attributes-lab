885ace412ff80e6f4c614929b92ccfdf808f4d65
Devante Lowery <dlowery2120@gmail.com>
2021-02-23 14:13:13 -0600

    commit tree ba82d38f4c4fcf374d2abf1c0937ca78fab4011b

---
+++ lib/dog.rb
@@ -0,0 +1,55 @@
+# class Dog
+#     def name
+#         @name
+#     end
+#     def name=(name)
+#         @name = name
+#     end
+#     def breed
+#         @breed
+#     end
+#     def breed=(breed)
+#         @breed = breed
+#     end
+# end
+
+
+attr_accessor(:name, :breed)
+
+
+
+# ## Objectives 
+
+# 1. Define ruby classes.
+# 2. Define methods that read from and write to instance variables.
+# 3. Create object properties using methods and instance variables.
+
+# ## Overview
+
+# This lab is all about using instance variables within a class to create methods that represent attributes or properties of an object.
+
+# ## Instructions
+
+# Get started with this lab by clicking the "Fork" button, cloning, and running `learn test`.
+
+# ## `Dog` and `lib/dog.rb`
+
+# ### Give a Dog a Name
+
+# You'll be teaching `Dog` about their names through two methods, `#name`, and `#name=` that read and write to a corresponding instance variable `@name`.
+
+# ### Give a Dog a Breed
+
+# ![Dog Breeds](https://curriculum-content.s3.amazonaws.com/module-1/ruby-oo-fundamentals/object-attributes-lab/Image_142_CommonDogBreeds.png)
+
+# You'll be teaching `Dog` about their breed through two methods, `#breed`, and `#breed=` that read and write to a corresponding instance variable `@breed`.
+
+# ## `Person` and `lib/person.rb`
+
+# ### Give a Person a Name
+
+# You'll be teaching `Person` about their names through two methods, `#name`, and `#name=` that read and write to a corresponding instance variable `@name`.
+
+# ### Give a Person a Job
+
+# You'll be teaching `Person` about their jobs through two methods, `#job`, and `#job=` that read and write to a corresponding instance variable `@job`.
